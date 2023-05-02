8d90a35311ac468c605a8234c33be2ea381a4957
JanathanPlanas <59850939+JanathanPlanas@users.noreply.github.com>
2023-05-01 21:41:07 -0300

    class pytorch practice

--- pytorch_classification.py
+++ pytorch_classification.py
@@ -1,115 +1,187 @@
+# Import dependencies
 
+from helper_functions import plot_decision_boundary, plot_predictions
+import matplotlib.pyplot as plt
 import torch
-import torch.nn as nn
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from torch import nn
+
+# Set the hyperparameters for data creation
+NUM_CLASSES = 6
+NUM_FEATURES = 2
+RANDOM_SEED = 42
+
+# 1. Create multi-class data
+X_blob, y_blob = make_blobs(n_samples=5000,
+                            n_features=NUM_FEATURES,  # X features
+                            centers=NUM_CLASSES,  # y labels
+                            # give the clusters a little shake up (try changing this to 1.0, the default)
+                            cluster_std=1.5,
+                            random_state=RANDOM_SEED
+                            )
+
+# 2. Turn data into tensors
+X_blob = torch.from_numpy(X_blob).type(torch.float)
+y_blob = torch.from_numpy(y_blob).type(torch.LongTensor)
+print(X_blob[:5], y_blob[:5])
+
+# 3. Split into train and test sets
+X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob,
+                                                                        y_blob,
+                                                                        test_size=0.2,
+                                                                        random_state=RANDOM_SEED
+                                                                        )
+
+# 4. Plot data
+plt.figure(figsize=(10, 7))
+plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
+plt.show()
+# Create device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
 
 
-class GamblerLoss(nn.Module):
-    def __init__(self, inference_will=1.1):
+# Build model
+class BlobModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+        """Initializes all required hyperparameters for a multi-class classification model.
+
+        Args:
+            input_features (int): Number of input features to the model.
+            out_features (int): Number of output features of the model
+              (how many classes there are).
+            hidden_units (int): Number of hidden units between layers, default 8.
+        """
         super().__init__()
-        """ Returns a CrossEntropyLoss-like object that has been modified
-            according to the DeepGambler paper [1].
-        Parameters
-        ----------
-        inference_will: floating point
-            The willingness to infer or abstain.  Greater values make the
-            inference more confident, while lower values make abstention more
-            likely.  Given m different classes (not inclusive of the abstention
-            class), meaningful values are in the range (1, m).
-        Notes
-        -----
-        [1] DeepGamblers: Learning to Abstain with Portfolio Theory. L. Ziyin,
-            Z. T. Wang, P. Pu Liang, R. Salakhutdinov, L.P. Morency, M. Ueda.
-            NeurIPS, 2019
-        """
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            # nn.ReLU(), # <- does our dataset require non-linear layers? (try uncommenting and see if the results change)
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            # nn.ReLU(), # <- does our dataset require non-linear layers? (try uncommenting and see if the results change)
+            # how many classes are there?
+            nn.Linear(in_features=hidden_units, out_features=output_features),
+        )
 
-        self.o = torch.tensor(inference_will)
-        self.nllloss = nn.NLLLoss()
-        self.log_softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, v, labels):
-        """ Returns the Gambler loss value
-        Parameters
-        ----------
-        v: a tensor of inputs with dimension BxI
-        labels: a tensor with dimension Bx1 of indexes in [0, I-2] of the
-                correct label (the I-1-th class is the abstinence one and
-                should not be used in training).
-        Notes
-        -----
-        This loss is a Negative Log Likelihood of weighted and scaled
-        probabilities (from the Softmax response).  We refer to the log of
-        these values as log_gambler_values.
-        """
-
-        x = self.log_gambler_values(v)
-        return self.nllloss(x, labels)
-
-    def log_gambler_values(self, v):
-        """ Returns the log values to be passed to the Negative Log Likelihood
-            Loss.
-        Parameters
-        ----------
-        v: a tensor of inputs with dimension BxI
-        Notes
-        -----
-        In order to avoid overflow issues due to exp of a large number, we
-        execute it only on arguments lower or equal than 0. In order to avoid
-        instability issues due to log of a small number, we execute it only on
-        arguments greater or equal than 1.
-        Our target values are in the form:
-            xi = log (fi*o + fm)
-        where fi = exp(vi)/sum(exp(vj)).
-        To avoid computing exponential with argument greater than 0 we rescale
-        the values wrt. the maximum per the first dimension, indicated as V.
-        => fi = exp(vi-V)/sum(exp(vj-V)).
-        xi = log[o*exp(vi-V)/sum(exp(vj-V)) + exp(vm-V)/sum(exp(vj-V))]
-        vm is the value of the m+1-th class, the abstinence class.
-        xi = log[(o*exp(vi-V) + exp(vm-V))/sum(exp(vj-V))]
-           = log[o*exp(vi-V)(1 + exp(vm-V)/(exp(vi-V)*o)] - log(sum(exp(vj-V))
-           = vi - V + log(o + exp(vm-V)/exp(vi-V)) - log(sum(exp(vj-V))
-           = log(o + exp(vm-V)/exp(vi-V)) + log_softmax(v)
-        The two exponential must be computed separately as there is no
-        guarantee that vm-vi<=0. Being the exponentials greater than 0 and o
-        >= 1, it follows the stability constraints are respected.
-        """
-        maxs = v.max(dim=1)[0]
-        lasts = v[:,-1]
-
-        upper = torch.exp(lasts-maxs)
-        upside = torch.exp(v.transpose(0, 1).add(-maxs)).div(upper).transpose(0, 1)
-        x = 1./upside + self.o
-        return self.log_softmax(v) + torch.log(x)
+    def forward(self, x):
+        return self.linear_layer_stack(x)
 
 
-def log_gambler(v, o):
-    """
-    dummy implementation for testing
-    """
-    softmax = nn.Softmax(dim=1)
-    x = softmax(v)
-    z = x[:, -1]
-    y = torch.mul(x, o)
-    x = y+z.unsqueeze(1)
+# Create an instance of BlobModel and send it to the target device
+model_4 = BlobModel(input_features=NUM_FEATURES,
+                    output_features=NUM_CLASSES,
+                    hidden_units=8).to(device)
 
-    x = torch.log(x)  ## could be numerically unstable if probabilities approach zero
-    return x
+print(model_4)
 
+# Create loss and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model_4.parameters(),
+                            lr=0.03)  # exercise: try changing the learning rate here and seeing what happens to the model's performance
 
-def output2class(model_output, coverage, abstain_class):
-    probs = torch.softmax(model_output, dim=1)[:, abstain_class]
-    _, predicted = torch.max(model_output[:, :abstain_class], dim=1)
-    predicted = (probs < coverage).int().mul(predicted)
-    predicted += (probs >= coverage).int().mul(abstain_class)
-    return predicted
+# Perform a single forward pass on the data (we'll need to put it to the target device for it to work)
+model_4.forward(X_blob_train.to(device))[:5]
 
 
-if __name__ == "__main__":
-    v = torch.tensor([[1, 2, 3], [6, 4, 5]], dtype=float)
-    o = 1.5
-    gl = GamblerLoss(o)
+# Make prediction logits with model
+y_logits = model_4(X_blob_test.to(device))
 
-    y = gl.log_gambler_values(v)
-    z = log_gambler(v, o)
+# Perform softmax calculation on logits across dimension 1 to get prediction probabilities
+y_pred_probs = torch.softmax(y_logits, dim=1)
+print(y_logits[:5])
+print(y_pred_probs[:5])
 
-    print(f"Expected {z}")
-    print(f"Computed {y}")
+
+# Which class does the model think is *most* likely at the index 0 sample?
+print(y_pred_probs[0])
+print(torch.argmax(y_pred_probs[0]))
+
+
+# Fit the model
+torch.manual_seed(42)
+
+# Set number of epochs
+epochs = 1000
+
+# Put data to target device
+X_blob_train, y_blob_train = X_blob_train.to(device), y_blob_train.to(device)
+X_blob_test, y_blob_test = X_blob_test.to(device), y_blob_test.to(device)
+
+# Calculate accuracy (a classification metric)
+
+
+def accuracy_fn(y_true, y_pred):
+    # torch.eq() calculates where two tensors are equal
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
+for epoch in range(epochs):
+    # Training
+    model_4.train()
+
+    # 1. Forward pass
+    y_logits = model_4(X_blob_train)  # model outputs raw logits
+    # go from logits -> prediction probabilities -> prediction labels
+    y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+    # print(y_logits)
+    # 2. Calculate loss and accuracy
+    loss = loss_fn(y_logits, y_blob_train)
+    acc = accuracy_fn(y_true=y_blob_train,
+                      y_pred=y_pred)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    # Testing
+    model_4.eval()
+    with torch.inference_mode():
+        # 1. Forward pass
+        test_logits = model_4(X_blob_test)
+        test_pred = torch.softmax(test_logits, dim=1).argmax(dim=1)
+        # 2. Calculate test loss and accuracy
+        test_loss = loss_fn(test_logits, y_blob_test)
+        test_acc = accuracy_fn(y_true=y_blob_test,
+                               y_pred=test_pred)
+
+    # Print out what's happening
+    if epoch % 10 == 0:
+        print(
+            f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%")
+
+
+# Make predictions
+model_4.eval()
+with torch.inference_mode():
+    y_logits = model_4(X_blob_test)
+
+# View the first 10 predictions
+print(y_logits[:10])
+
+
+# Turn predicted logits in prediction probabilities
+y_pred_probs = torch.softmax(y_logits, dim=1)
+
+# Turn prediction probabilities into prediction labels
+y_preds = y_pred_probs.argmax(dim=1)
+
+# Compare first 10 model preds and test labels
+print(f"Predictions: {y_preds[:10]}\nLabels: {y_blob_test[:10]}")
+print(f"Test accuracy: {accuracy_fn(y_true=y_blob_test, y_pred=y_preds)}%")
+
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_4, X_blob_train, y_blob_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_4, X_blob_test, y_blob_test)
+plt.show()
